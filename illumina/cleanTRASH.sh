5bb1355f507d241274ceaedfa58a1085e404218c
Robert Bjornson <robert.bjornson@yale.edu>
2021-02-15 16:06:33 -0500

    updated cleanTRASH.sh for new archive system

--- illumina/cleanTRASH.sh
+++ illumina/cleanTRASH.sh
@@ -1,6 +1,28 @@
+#!/bin/bash
 
-for d in /gpfs/ycga/sequencers/panfs/TRASH/sequencers*/sequencer?/runs/[0-9][0-9]* \
-         /gpfs/ycga/sequencers/illumina/TRASH/sequencer?/runs/[0-9][0-9]*
-do
-   echo $(date): Deleting $d >> cleanTRASH.log
+set -e
+
+for d in $(ls -1d /gpfs/ycga/sequencers/illumina/TRASH/sequencer?*/runs/[12]*) 
+do 
+  echo "considering $d"
+
+  arch=${d/\/TRASH/}
+  #arch=${arch/\/gpfs\/ycga\//\/SAY\/archive\/YCGA-729009-YCGA\/archive\/ycga-gpfs/}
+  arch=${arch/\/gpfs\/ycga\//\/SAY\/archive\/YCGA-729009-YCGA-A2\/archive\/ycga-gpfs/}
+
+  fin="$arch/$(basename $arch)_finished.txt"
+  echo $fin
+  [ ! -f $fin ] && (echo "$fin does not exist!"; exit 1)
+
+  tf="$arch/$(basename $arch)_0.tar"
+  echo $tf
+  #[ ! -f $tf ] && (echo "$tf does not exist!"; exit 1)
+
+  [[ $(find $tf -type f -size +10000c 2>/dev/null) ]] ||  (echo "$tf does not exist or too small!"; exit 1)
+
+  if [ "$1" = "--delete" ]
+  then
+    echo "deleting $d"
+    rm -rf $d
+  fi
 done
